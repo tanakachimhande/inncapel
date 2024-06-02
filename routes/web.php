efb437feb294c5b8d108ec43bb2698753b37741b
Tanaka Chimhande <90509789+tanakachimhande@users.noreply.github.com>
2024-06-02 14:43:20 +0200

    controller imported

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\dashboard;
 use App\Http\Controllers\login;
+use App\Http\Controllers\ContactController;
 
 
 // Website Pages Routes
